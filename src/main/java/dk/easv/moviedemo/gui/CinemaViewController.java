26f22da2917f48ac3d05ea5c1a0c6e1d23726c18
hlacis <jakubhlacik@seznam.cz>
2026-01-13 15:49:31 +0100

    Rebuilt CinemaViewController method with JavaFX rule for consideration. + volume button works now

--- src/main/java/dk/easv/moviedemo/gui/CinemaViewController.java
+++ src/main/java/dk/easv/moviedemo/gui/CinemaViewController.java
@@ -2,7 +2,7 @@ package dk.easv.moviedemo.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -10,36 +10,54 @@ import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.stage.Stage;
 
 import java.io.File;
 
 public class CinemaViewController {
+
     @FXML
     private Pane paneId;
+
     @FXML
     private BorderPane borderpaneId;
 
     @FXML
     private HBox controlsId;
-    
-    
+
+    @FXML
+    private Slider sliderVolume;
+
     private MediaPlayer mediaPlayer;
     private MediaView mediaView;
-    public CinemaViewController()
-    {
-        MainWindowController mainWindowController = new MainWindowController();
-        Media media = new Media(new File(mainWindowController.getSelectedItem()).toURI().toString());
 
-        mediaPlayer = new MediaPlayer(media);
-        this.mediaView = new MediaView(mediaPlayer);
-        this.mediaPlayer = mediaPlayer;
+    public CinemaViewController() {
     }
-    public void initialize()
-    {
+
+    @FXML
+    public void initialize() {
+
+        controlsId.setOpacity(0);
+
+        mediaView = new MediaView();
         borderpaneId.setCenter(mediaView);
-        borderpaneId.autosize();
+
+        mediaView.setPreserveRatio(true);
+        mediaView.fitWidthProperty().bind(borderpaneId.widthProperty());
+        mediaView.fitHeightProperty().bind(borderpaneId.heightProperty());
+
+        String url = new File("video.mp4").toURI().toString();
+        Media media = new Media(url);
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+
+        mediaPlayer.setVolume(sliderVolume.getValue() / 100.0);
+        sliderVolume.valueProperty().addListener((obs, oldV, newV) ->
+                mediaPlayer.setVolume(newV.doubleValue() / 100.0)
+        );
+
+        mediaPlayer.setOnReady(mediaPlayer::play);
     }
+
     public void OnMouseMoved(MouseEvent mouseEvent) {
         controlsId.setOpacity(1);
     }
@@ -50,7 +68,6 @@ public class CinemaViewController {
 
     public void btnPlayOnClick(ActionEvent actionEvent) {
         mediaPlayer.play();
-
     }
 
     public void btnPauseOnClick(ActionEvent actionEvent) {
